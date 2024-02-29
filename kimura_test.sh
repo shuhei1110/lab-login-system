7998e816e2babb89215414a01958344138cf476d
Mimi315 <mucunyoshikazu@gmail.com>
2024-02-29 12:36:09 +0900

    実験1

--- kimura_test.sh
+++ kimura_test.sh
@@ -1,1 +1,19 @@
-hoge
+#!/bin/bash
+
+bluetoothctl
+echo "hello1"
+devices_stdout = $(paired-device)
+echo "hello2"
+for item "${devices_stdout}";do
+    connect_stdout = $(connect "${item:7:24}")
+    if $?;do
+        exit
+    echo "hello3"
+#    if [[${connect_stdout:0:5} == *[CHG]*]];then
+#        curl
+#        echo "hello4"
+#    else
+#        curl
+#    
+    disconnect "${item:7:24}"
+    echo "hello4"

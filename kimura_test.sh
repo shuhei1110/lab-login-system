a2616c28a2c85f6d6da3d10ae34a96e0ff9f96f8
Mimi315 <mucunyoshikazu@gmail.com>
2024-02-29 15:12:32 +0900

    実験10

--- kimura_test.sh
+++ kimura_test.sh
@@ -6,8 +6,14 @@ function paired_devices() {
   } | bluetoothctl | grep "Device " | sed -r 's/^.*(([0-9A-F]{2}:){5}[0-9A-F]{2}).*$/\1/'
 }
 
+function is_connected(){
+    {
+        printf "connect $line"
+    } | bluetoothctl | grep "Connected: "
+}
+
 paired_devices | while read line
 do
     echo $line
-    echo "check point"
+    is_connected
 done

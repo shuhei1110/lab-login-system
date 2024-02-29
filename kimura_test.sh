7a9fe4ef79e3f8b542842bc4f398876d6503563a
Mimi315 <mucunyoshikazu@gmail.com>
2024-02-29 14:21:16 +0900

    実験3

--- kimura_test.sh
+++ kimura_test.sh
@@ -8,22 +8,13 @@ function paired_devices() {
 
 paired_devices
 
-#bluetoothctl
-
 #devices_stdout = $(paired-devices)
 #echo "${devices_stdout}"
 
-#for item "${devices_stdout}";do
-#    connect_stdout = $(connect "${item:7:24}")
-#    if $?;do
-#        exit
-    
-#    if [[${connect_stdout:0:5} == *[CHG]*]];then
-#        curl
-#        echo "hello4"
-#    else
-#        curl
-#    
-#    disconnect "${item:7:24}"
-#    exit
+for item "${paired_devices}";do
+    bluetoothctl connect "${item:7:24}" | grep "[CHG]" | 
+    echo $?
+
+    bluetoothctl disconnect "${item:7:24}"
+    exit
     

8bbf82368c1cfb016406cee7b7df7ae2a26163ff
Mimi315 <mucunyoshikazu@gmail.com>
2024-02-29 18:19:39 +0900

    実験13

--- kimura_test.sh
+++ kimura_test.sh
@@ -5,7 +5,7 @@ function paired_devices() {
     printf "paired-devices\n\n"
     } | bluetoothctl | grep "Device " | sed -r 's/^.*(([0-9A-F]{2}:){5}[0-9A-F]{2}).*$/\1/'
 }
-timeout=5
+timeout=10
 paired_devices | while read line
 do
     echo $line

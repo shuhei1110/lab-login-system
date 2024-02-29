88cd0a0cf06afb2279635b4fd2daf6d34c4fd060
Mimi315 <mucunyoshikazu@gmail.com>
2024-02-29 16:06:08 +0900

    実験10

--- kimura_test.sh
+++ kimura_test.sh
@@ -6,10 +6,14 @@ function paired_devices() {
     } | bluetoothctl | grep "Device " | sed -r 's/^.*(([0-9A-F]{2}:){5}[0-9A-F]{2}).*$/\1/'
 }
 
+is_connected(){
+    bluetoothctl connect $line | grep "yes"
+}
+
 paired_devices | while read line
 do
     echo $line
-    con_log='bluetoothctl connect $line'
+    con_log='is_connected'
     echo ${con_log}
     bluetoothctl disconnect $line
 done

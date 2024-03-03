3b1b8a5bf4f6520be87d9e68d29888333470b5bd
shuhei <68697620+shuhei1110@users.noreply.github.com>
2024-03-03 18:00:59 +0900

    Update notion_api_ctl.py

--- utils/notion_api_ctl.py
+++ utils/notion_api_ctl.py
@@ -16,7 +16,7 @@ DATABASE_ID = settings.DATABASE_ID
 API_URL = settings.API_URL
 headers = settings.headers
 
-log_path = os.environ.get("LLS_PATH") + "logs/" + datetime.now().strftime('%Y%m%d') + ".log"
+log_path = os.environ.get("LLS_PATH") + "/logs/" + datetime.now().strftime('%Y%m%d') + ".log"
 addresses = log_file_ctl.analyze_log(log_path)
 
 def print_datetime(f):

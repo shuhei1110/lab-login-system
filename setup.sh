f9ace232fee496815fdc00284899899e22efd6ea
Mimi315 <mucunyoshikazu@gmail.com>
2024-03-03 17:27:58 +0900

    setup.sh 追加

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+sudo apt -y install bluetooth bluez
+
 export LLS_PATH=$(pwd)
+
+crontab -l > mycron 2>/dev/null
+echo "LANG=ja_JP.UTF-8" >> mycron
+echo "PATH=$PATH" >> mycron
+echo "*/10 * * * * $LLS_PATH/scan.sh" >> mycron
+crontab mycron
+rm mycron
+
+sudo systemctl restart cron

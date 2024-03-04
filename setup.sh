69c89a8236307ea95cc83524eaf6f98758b17310
shuhei1110 <s18s411g@gmail.com>
2024-03-04 14:30:58 +0900

    setup.shで.logを作成するスクリプトを追加

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ sudo apt -y install bluetooth bluez
 
 export LLS_PATH=$(pwd)
 
+current_date=$(date +"%Y%m%d")
+log_file_name="${current_date}.log"
+touch "$log_file_name"
+
 crontab -l > mycron 2>/dev/null
 echo "LANG=ja_JP.UTF-8" >> mycron
 echo "PATH=$PATH" >> mycron

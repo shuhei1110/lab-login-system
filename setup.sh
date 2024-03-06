fc77f0d17b9f2b3f8d64af9854d348977dd9fb91
shuhei1110 <s18s411g@gmail.com>
2024-03-06 18:00:55 +0900

    edit setup.py

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ export LLS_PATH=$(pwd)
 crontab -l > mycron 2>/dev/null
 echo "LANG=ja_JP.UTF-8" >> mycron
 echo "PATH=$PATH" >> mycron
-echo "*/1 * * * * /usr/bin/python $LLS_PATH/scan.sh" >> mycron
+echo "*/1 * * * * /usr/bin/python $LLS_PATH/scan.py" >> mycron
 crontab mycron
 rm mycron
 

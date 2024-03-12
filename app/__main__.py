d79df894c8d9a91c7152d49c9801fa53c4f1eeba
shuhei1110 <s18s411g@gmail.com>
2024-03-12 16:07:21 +0900

    edit __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -6,48 +6,39 @@ from config import config
 
 def print_datetime(f):
     def wrapper(*args, **kwargs):
-        print(f'--- Start process: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} ---')
+        print(f'\033[34m--- Start process: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} ---\033[0m')
         f(*args, **kwargs)
-        print(f'--- End process: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} ---')
+        print(f'\033[34m--- End process: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} ---\033[0m')
     return wrapper
 
 @print_datetime
 def main():
     APP_PATH = config.APP_PATH
 
-    # 現在の日時を取得
     current_date = datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
     log_date = datetime.now().strftime('%Y%m%d')
 
     log_file_path = APP_PATH + "/logs/" + log_date + ".log"
 
-    # ログファイルに日時を書き込み，ファイルがなければ作成
     with open(log_file_path, 'a') as log_file:
         log_file.write(current_date)
 
-    # ペアリングされているデバイスを取得
-    def paired_devices():
-        command_output = subprocess.check_output(['bluetoothctl', 'devices', 'Paired']).decode('utf-8')
-        devices = [line.split()[1] for line in command_output.splitlines() if 'Device' in line]
-        return devices
+    command_output = subprocess.check_output(['bluetoothctl', 'devices', 'Paired']).decode('utf-8')
+    devices = [line.split()[1] for line in command_output.splitlines() if 'Device' in line]
 
-    # デバイスごとに処理
-    for device in paired_devices():
+    for device in devices:
 
-        # デバイスへの接続を試みる
         try:
             result = subprocess.run(['sudo', 'l2ping', '-c', '1', '-t', '5', device], check=True, capture_output=True)
             stdout_txt = result.stdout.decode('utf-8')
-            # print(stdout_txt)
 
             if "received" in stdout_txt:
                 print(f'{device}を検出')
-                # 接続成功時にログにデバイスを書き込み
                 with open(log_file_path, 'a') as log_file:
                     log_file.write(f', {device}')
 
             else:
-                print(f'{device}は検出不可')
+                print(f'{device}検出不可')
             
         except subprocess.CalledProcessError as e:
             print(f'{device}検出不可')
